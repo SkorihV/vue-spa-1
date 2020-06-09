19f100ca8ac8aaf9be890cb2d5a7125459d9f6c8
skorihv <skorihv@mail.ru>
2020-06-09 17:39:58 +0700

    Добавил возможность создания аккаунта через почту

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -77,7 +77,7 @@ export default {
           email: this.email,
           password: this.password
         };
-        console.log(user);
+        this.$store.dispatch("registerUser", user);
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import fb from 'firebase'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -15,5 +16,17 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created() {
+    fb.initializeApp({
+      apiKey: "AIzaSyAg-acRVRvEhk8ApMc-E_qb5jUMoWxC1dE",
+      authDomain: "its-ads-d2d35.firebaseapp.com",
+      databaseURL: "https://its-ads-d2d35.firebaseio.com",
+      projectId: "its-ads-d2d35",
+      storageBucket: "its-ads-d2d35.appspot.com",
+      messagingSenderId: "244831025837",
+      appId: "1:244831025837:web:f5f17d6becfaaf5820a424",
+      measurementId: "G-RTQ18X7223"
+    })
+  }
 })
